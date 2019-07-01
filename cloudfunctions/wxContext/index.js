f762ee36ad457e96aa4f1964c59d62ad5b87d962
elco45 <dennischeong2003@yahoo.es>
2019-07-01 01:27:02 -0400

    Renaming cloudfunction login to wxContext

---
+++ cloudfunctions/wxContext/index.js
@@ -0,0 +1,12 @@
+const cloud = require('wx-server-sdk');
+cloud.init();
+
+exports.main = event => {
+  const wxContext = cloud.getWXContext();
+  return {
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+  };
+};
