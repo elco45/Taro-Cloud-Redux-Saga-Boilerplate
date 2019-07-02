395583ed13f9cfc233296ed7106c4bc1ab4c161a
elco45 <dennischeong2003@yahoo.es>
2019-07-02 00:15:40 -0400

    Adding no dangling underscore rule to allow _id

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,6 +62,7 @@ module.exports = {
     'newline-per-chained-call': 0,
     'no-confusing-arrow': 0,
     'no-console': 1,
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
     'no-unused-vars': ['error', { varsIgnorePattern: 'Taro' }],
     'no-use-before-define': 0,
     'prefer-template': 2,
